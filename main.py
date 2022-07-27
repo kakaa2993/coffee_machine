fe9bcbe3ee2d59dceff335a41ea2dc98ae5a3be8
kakaa2993@ <kakaa2993@gmail.com>
2022-07-27 06:28:33 -0700

    add todo tasks that need to do

--- main.py
+++ main.py
@@ -23,3 +23,28 @@ MENU = {
         "cost": 3.0,
     }
 }
+# The stock:
+WATER = 300  # ml
+MILK = 200  # ml
+COFFEE = 100  # g
+MONEY = 0  # $0
+coffee_emoji = '☕'
+
+# TODO: The user can chose between: off ( to turn of the machine),
+# TODO (espresso/latte/cappuccino),
+# TODO report(to see the total report of the machine)
+
+orders = {
+    "off": "to exit the program",
+    "espresso": "to order espresso",
+    "latte": " to order espresso",
+    "cappuccino": " to order cappuccino",
+    "report": " to see the report of the machine"
+}
+# TODO: Check resources sufficient?
+
+
+# TODO Process coins.
+
+
+# TODO Make Coffee
